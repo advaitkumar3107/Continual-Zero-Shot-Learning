1aca3af7fd2e96f896de74e2afd87953094dcc68
advaitkumar3107 <52429038+advaitkumar3107@users.noreply.github.com>
2021-05-19 11:34:47 +0530

    Removed parser

--- model.py
+++ model.py
@@ -2,18 +2,15 @@ import torch
 from torch import nn
 from models import resnet
 
-def generate_model(opt):
-    assert opt.model in [
-        'resnet', 'resnet2p1d', 'preresnet', 'wideresnet', 'resnext', 'densenet'
-    ]
-    model = resnet.generate_model(model_depth=opt.model_depth,
-                                      n_classes=opt.n_classes,
+def generate_model():
+    model = resnet.generate_model(model_depth = 50,
+                                      n_classes = 1139,
                                       n_input_channels = 3,
-                                      shortcut_type=opt.resnet_shortcut,
-                                      conv1_t_size=opt.conv1_t_size,
-                                      conv1_t_stride=opt.conv1_t_stride,
-                                      no_max_pool=opt.no_max_pool,
-                                      widen_factor=opt.resnet_widen_factor)
+                                      shortcut_type = 'B',
+                                      conv1_t_size = 7,
+                                      conv1_t_stride = 1,
+                                      no_max_pool = False,
+                                      widen_factor = 1.0)
     return model
 
 
