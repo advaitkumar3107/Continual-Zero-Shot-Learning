54b5f570bf864b5b20ed7696ccf98df7fefbf9cc
advaitkumar3107 <52429038+advaitkumar3107@users.noreply.github.com>
2021-05-24 12:39:00 +0530

    resnet 18

--- model.py
+++ model.py
@@ -3,8 +3,8 @@ from torch import nn
 from models import resnet
 
 def generate_model():
-    model = resnet.generate_model(model_depth = 50,
-                                      n_classes = 700,
+    model = resnet.generate_model(model_depth = 18,
+                                      n_classes = 1039,
                                       n_input_channels = 3,
                                       shortcut_type = 'B',
                                       conv1_t_size = 7,
