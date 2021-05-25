b1e2a85048803a903341162b680257ae713dd77c
advaitkumar3107 <52429038+advaitkumar3107@users.noreply.github.com>
2021-05-26 00:04:49 +0530

    Load sizes

--- dataloader.py
+++ dataloader.py
@@ -9,23 +9,28 @@ def create_data_loader(feat_path, classes, batch_size = 100):
     labels = feats['labels']
     feats = feats['features']
     feats = np.transpose(feats,(1,0))
+    labels = np.squeeze(labels, axis = 1)
 
-    train_data, test_data, train_label, test_label = train_test_split(feats, labels, test_size = 0.25, shuffle = False)
+    train_data, test_data, train_label, test_label = train_test_split(feats, labels, test_size = 0.25, random_state = 42)
 
-    train_label = torch.tensor(np.squeeze(train_label, axis = 1))
-    test_label = torch.tensor(np.squeeze(test_label, axis = 1))
+    train_label = torch.tensor(train_label)
+    test_label = torch.tensor(test_label)
 
     data = []
+    len_train = 0
+    len_test = 0
     for i in range(len(train_label)):
         if (train_label[i] in classes):
             data.append([train_data[i,:], train_label[i]])
+            len_train += 1
 
     data_test = []
     for i in range(len(test_label)):
         if (test_label[i] in classes):
             data_test.append([test_data[i,:], test_label[i]])
+            len_test += 1
 
     train_loader = DataLoader(data, batch_size = batch_size, shuffle = True)
     test_loader = DataLoader(data_test, batch_size = batch_size, shuffle = False)
 
-    return train_loader, test_loader
+    return train_loader, test_loader, len_train, len_test
