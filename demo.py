4f75c21a133285d9427a6cb0a937907a1483095c
advaitkumar3107 <52429038+advaitkumar3107@users.noreply.github.com>
2021-05-31 22:42:48 +0530

    Check for attribute vector

--- demo.py
+++ demo.py
@@ -21,13 +21,10 @@ from torch.autograd import Variable
 import requests
 from video_data_loader import video_dataset
 from dataloader import create_data_loader
+import scipy.io as sio
 
-all_classes = range(10)
-train_dataloader, test_dataloader, len_train, len_test = create_data_loader('ucf101_i3d/i3d.mat', all_classes[:10])
+feats = sio.loadmat('../npy_files/seen_semantic_51.npy')
+att = feats['att']
 
-sum = 0
-
-for (inputs, labels) in train_dataloader:
-    sum += (labels == 0).sum()
-
-print(sum)    
+print(feats)    
+print(att.shape)
