6177e7bd0ccf2ccbedc7556e2b6fc33b10b8398c
advaitkumar3107 <52429038+advaitkumar3107@users.noreply.github.com>
2021-06-03 11:42:55 +0530

    Added dataloading for old classes

--- dataloader.py
+++ dataloader.py
@@ -34,3 +34,41 @@ def create_data_loader(feat_path, classes, batch_size = 100):
     test_loader = DataLoader(data_test, batch_size = batch_size, shuffle = False)
 
     return train_loader, test_loader, len_train, len_test
+
+def create_old_data_loader(feat_path, classes, batch_size = 100, num_samples = 10):
+    feats = sio.loadmat(feat_path)
+    labels = feats['labels'] - 1
+    feats = feats['features']
+    feats = np.transpose(feats,(1,0))
+    labels = np.squeeze(labels, axis = 1)
+
+    train_data, test_data, train_label, test_label = train_test_split(feats, labels, test_size = 0.25, random_state = 42)
+
+    train_label = torch.tensor(train_label)
+    test_label = torch.tensor(test_label)
+
+    data = []
+    len_train = 0
+    len_test = 0
+
+    num_classes = classes[-1]
+    samples = torch.ones((num_classes+1))*num_samples
+    
+    for i in range(len(train_label)):
+        if (train_label[i].item() in classes):
+            if (samples[train_label[i].item()] > 0):
+                data.append([train_data[i,:], train_label[i]])
+                samples[train_label[i].item()] -= 1
+                len_train += 1
+
+    data_test = []
+    for i in range(len(test_label)):
+        if (test_label[i] in classes):
+            data_test.append([test_data[i,:], test_label[i]])
+            len_test += 1
+
+    train_loader = DataLoader(data, batch_size = batch_size, shuffle = True)
+    test_loader = DataLoader(data_test, batch_size = batch_size, shuffle = False)
+
+    return train_loader, test_loader, len_train, len_test
+    
