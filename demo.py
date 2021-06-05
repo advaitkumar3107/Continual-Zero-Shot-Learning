4eef83ab4b8850d2f5216d0de3a84eca5606fac1
advaitkumar3107 <52429038+advaitkumar3107@users.noreply.github.com>
2021-06-06 01:18:26 +0530

    Check feature loading for HMDB51

--- demo.py
+++ demo.py
@@ -24,14 +24,12 @@ from dataloader import create_data_loader, create_old_data_loader
 import scipy.io as sio
 
 
+atts = sio.loadmat('hmdb_i3d/i3d.mat')
+feats = atts['labels']
+print(feats.shape)
 #classes = range(101)
 
 #train_loader, test_loader, len_train, len_test = create_old_data_loader('ucf101_i3d/i3d.mat', classes[90:])
 
 #for (inputs, labels) in test_loader:
     #print(inputs.shape)
-
-model = nn.BatchNorm1d(100)
-tens = torch.ones((1,100))
-output = model(tens)
-print(output.shape)
