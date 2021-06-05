98406691331f4b904676f5e01955f9452548a363
advaitkumar3107 <52429038+advaitkumar3107@users.noreply.github.com>
2021-06-05 22:32:10 +0530

    Check loading of correct features

--- demo.py
+++ demo.py
@@ -24,8 +24,14 @@ from dataloader import create_data_loader, create_old_data_loader
 import scipy.io as sio
 
 
-classes = range(70)
+#classes = range(101)
 
-train_loader, test_loader, len_train, len_test = create_old_data_loader('ucf101_i3d/i3d.mat', classes[:50])
+#train_loader, test_loader, len_train, len_test = create_old_data_loader('ucf101_i3d/i3d.mat', classes[90:])
 
-print(len_train)
+#for (inputs, labels) in test_loader:
+    #print(inputs.shape)
+
+model = nn.BatchNorm1d(100)
+tens = torch.ones((1,100))
+output = model(tens)
+print(output.shape)
