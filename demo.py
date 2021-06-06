55612c384630ff390ba71ce9b6bae31c29743bc4
advaitkumar3107 <52429038+advaitkumar3107@users.noreply.github.com>
2021-06-06 23:25:32 +0530

    Check loading of model state dict

--- demo.py
+++ demo.py
@@ -22,9 +22,11 @@ import requests
 from video_data_loader import video_dataset
 from dataloader import create_data_loader, create_old_data_loader
 import scipy.io as sio
+from models.nets import *
 
+checkpoint = torch.load('run/pipeline_incremental/Bi-LSTM-ucf101_increment_1_epoch-499.pth.tar',map_location=lambda storage, loc: storage)   # Load all tensors onto the CPU
 
-feat_path = 'hmdb_i3d/i3d.mat'
-train_dataloader, test_dataloader, len_train, len_test = create_data_loader(feat_path, range(20))
+state_dict = checkpoint['classifier_state_dict']
 
-print(len_train)
+classifier = Classifier(num_classes = 50)
+print(state_dict)
