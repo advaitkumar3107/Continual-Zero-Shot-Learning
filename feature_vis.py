4d0e9cf48f9ec74ec53d54b8e8712ef6c8d000ae
Dipesh Tamboli <dipeshtamboli@gmail.com>
2021-06-15 09:23:54 +0530

    Changed legend style and names

--- feature_vis.py
+++ feature_vis.py
@@ -1,48 +1,57 @@
 import os
-import torch
 import pdb
 import numpy as np
 from scipy import io
 # !pip install MulticoreTSNE
-#from MulticoreTSNE import MulticoreTSNE as TSNE
+# from MulticoreTSNE import MulticoreTSNE as TSNE
 from sklearn.manifold import TSNE
-from sklearn.decomposition import PCA
-import pandas as pd
 from matplotlib import pyplot as plt
 import matplotlib
 import seaborn as sns
 import time
-from models.nets import *
-from model import load_pretrained_model, generate_model
-from video_data_loader import video_dataset, old_video_dataset
-from sklearn.preprocessing import normalize
 
 num_classes = 10
+start_class = 60
 
-feat_path = "gen_features/episode_0/convlstm_feat_labs_" + str(num_classes) + "_0.npy"
+feat_path = f"convlstm_feat_labs_{num_classes}_{start_class}.npy"
 convlstm_feat = np.load(feat_path)
 
-gen_feat_path = "gen_features/episode_0/gen_feat_labs_" + str(num_classes) + "_0.npy"
+gen_feat_path = f"gen_feat_labs_{num_classes}_{start_class}.npy"
 gen_feat = np.load(gen_feat_path)
-
-convlstm_feat[:,:-1] = normalize(convlstm_feat[:,:-1])
-gen_feat[:,:-1] = normalize(gen_feat[:,:-1])
 
 print("Conv LSTM feature shape {}".format(convlstm_feat.shape))
 print("Generator feature shape {}".format(gen_feat.shape))
 
-x = np.concatenate((convlstm_feat, gen_feat), 0)
-y = x[:,-1]
-x = x[:,:-1]
+all_features = np.concatenate((convlstm_feat, gen_feat), 0)
+dataset_label = np.zeros((all_features.shape[0],1))
+dataset_style = np.zeros((all_features.shape[0],1))
 
-tsne = TSNE(n_components=2, verbose=1, perplexity=25, n_iter=1000, learning_rate=200)    
-embeddings = tsne.fit_transform(x)
+dataset_style[convlstm_feat.shape[0]:,:] = 1
+
+for i in range(all_features.shape[0]):
+    dataset_label[i,:] = all_features[i, -1]
+    # print(all_features[i,-1])
+
+#dataset_label[convlstm_feat.shape[0]:,:] = 1
+
+start_time = time.time()
+
+tsne = TSNE(n_jobs=16)
+
+embeddings = tsne.fit_transform(all_features)
+vis_x = embeddings[:, 0]
+vis_y = embeddings[:, 1]
+
 sns.set(rc={'figure.figsize':(11.7,8.27)})
-
-vis_x = embeddings[:,0]
-vis_y = embeddings[:,1]
 palette = sns.color_palette("bright", num_classes)
+# Legend symbols
+# https://matplotlib.org/stable/api/markers_api.html
 
-plot = sns.scatterplot(vis_x, vis_y, hue = y, legend = 'full', palette = palette)
-
+plot = sns.scatterplot(vis_x, vis_y, hue=dataset_label[:,0], style = dataset_style[:,0], markers=['P', 'o'], palette=palette)
+plot.get_legend().set_title("Classes")
+handles, labels = plot.get_legend_handles_labels()
+labels[-1] = "gen"
+labels[-2] = "conv"
+plot.legend(handles, labels) 
 plt.savefig("dataset_tsne.png")
+print("--- {} mins {} secs---".format((time.time() - start_time)//60,(time.time() - start_time)%60))
