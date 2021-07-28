44cd8484462fba47b12734fc81d3ce4d188f6365
advaitkumar3107 <52429038+advaitkumar3107@users.noreply.github.com>
2021-07-28 23:18:53 +0530

    remove function

--- clustering.py
+++ clustering.py
@@ -1,92 +1,55 @@
 from sklearn import metrics
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+
 import matplotlib.pyplot as plt
+import numpy as np
 
-def bench_k_means(kmeans, name, data, labels):
-    """Benchmark to evaluate the KMeans initialization methods.
+num_classes = 25
+start_class = 25
 
-    Parameters
-    ----------
-    kmeans : KMeans instance
-        A :class:`~sklearn.cluster.KMeans` instance with the initialization
-        already set.
-    name : str
-        Name given to the strategy. It will be used to show the results in a
-        table.
-    data : ndarray of shape (n_samples, n_features)
-        The data to cluster.
-    labels : ndarray of shape (n_samples,)
-        The labels used to compute the clustering metrics which requires some
-        supervision.
-    """
-    estimator = make_pipeline(StandardScaler(), kmeans).fit(data)
-    results = [name, fit_time, estimator[-1].inertia_]
+gen_feat_path = f"gen_features/episode_0/gen_feat_labs_{num_classes}_{start_class}.npy"
+gen_feat = np.load(gen_feat_path)
+data = gen_feat[:,:-1]
+labels = gen_feat[:,-1]
 
-    # Define the metrics which require only the true labels and estimator
-    # labels
-    clustering_metrics = [
-        metrics.homogeneity_score,
-        metrics.completeness_score,
-        metrics.v_measure_score,
-        metrics.adjusted_rand_score,
-        metrics.adjusted_mutual_info_score,
-    ]
-    results += [m(labels, estimator[-1].labels_) for m in clustering_metrics]
+print("Generator feature shape {}".format(gen_feat.shape))
 
-    # The silhouette score requires the full dataset
-    results += [
-        metrics.silhouette_score(data, estimator[-1].labels_,
-                                 metric="euclidean", sample_size=300,)
-    ]
-
-    # Show the results
-    formatter_result = ("{:9s}\t{:.3f}s\t{:.0f}\t{:.3f}\t{:.3f}"
-                        "\t{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}")
-    print(formatter_result.format(*results))
-
-if __name__ == "__main__":
-
-    num_classes = 25
-    start_class = 25
-
-    gen_feat_path = f"gen_features/episode_0/gen_feat_labs_{num_classes}_{start_class}.npy"
-    gen_feat = np.load(gen_feat_path)
-
-    print("Generator feature shape {}".format(gen_feat.shape))
-
-    reduced_data = PCA(n_components=2).fit_transform(gen_feat)
-    kmeans = KMeans(init="k-means++", n_clusters=10, n_init=4)
-    kmeans.fit(reduced_data)
+reduced_data = PCA(n_components=2).fit_transform(gen_feat)
+kmeans = KMeans(init="k-means++", n_clusters=10, n_init=4)
+bench_k_means(kmeans=kmeans, name="k-means++", data=data, labels=labels)
+kmeans.fit(reduced_data)
 
 # Step size of the mesh. Decrease to increase the quality of the VQ.
-    h = .02     # point in the mesh [x_min, x_max]x[y_min, y_max].
+h = .02     # point in the mesh [x_min, x_max]x[y_min, y_max].
 
 # Plot the decision boundary. For that, we will assign a color to each
-    x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1
-    y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:, 1].max() + 1
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1
+y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
 
 # Obtain labels for each point in mesh. Use last trained model.
-    Z = kmeans.predict(np.c_[xx.ravel(), yy.ravel()])
+Z = kmeans.predict(np.c_[xx.ravel(), yy.ravel()])
 
 # Put the result into a color plot
-    Z = Z.reshape(xx.shape)
-    plt.figure(1)
-    plt.clf()
-    plt.imshow(Z, interpolation="nearest",
+Z = Z.reshape(xx.shape)
+plt.figure(1)
+plt.clf()
+plt.imshow(Z, interpolation="nearest",
            extent=(xx.min(), xx.max(), yy.min(), yy.max()),
            cmap=plt.cm.Paired, aspect="auto", origin="lower")
 
-    plt.plot(reduced_data[:, 0], reduced_data[:, 1], 'k.', markersize=2)
+plt.plot(reduced_data[:, 0], reduced_data[:, 1], 'k.', markersize=2)
 # Plot the centroids as a white X
-    centroids = kmeans.cluster_centers_
-    plt.scatter(centroids[:, 0], centroids[:, 1], marker="x", s=169, linewidths=3,
+centroids = kmeans.cluster_centers_
+plt.scatter(centroids[:, 0], centroids[:, 1], marker="x", s=169, linewidths=3,
             color="w", zorder=10)
-    plt.title("K-means clustering on the digits dataset (PCA-reduced data)\n"
+plt.title("K-means clustering on the digits dataset (PCA-reduced data)\n"
           "Centroids are marked with white cross")
-    plt.xlim(x_min, x_max)
-    plt.ylim(y_min, y_max)
-    plt.xticks(())
-    plt.yticks(())
-    plt.show()
+plt.xlim(x_min, x_max)
+plt.ylim(y_min, y_max)
+plt.xticks(())
+plt.yticks(())
+plt.show()
